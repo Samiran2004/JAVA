e2fc1ea8de904e9153f5cc7f63ec3ee5702d7993
Samiran Samanta <samirans170@gmail.com>
2024-05-30 22:59:32 +0530

    Commit

--- Lecture-14/Final_Value.java
+++ Lecture-14/Final_Value.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Final_Value {
+import java.util.Scanner;
+
+public class Final_Value {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size: ");
+        int size = input.nextInt();
+        String[] operations = new String[size];
+        for(int i = 0; i<size; i++){
+            System.out.printf("Enter %d number operation: ",i+1);
+            operations[i] = input.next();
+        }
+        input.close();
+        System.out.println(finalValue(operations));
+    }
+
+    static int finalValue(String[] arr){
+//        int value = 0;
+//        for(int i = 0; i<arr.length; i++){
+//            if(arr[i].contains("+")){
+//                value += 1;
+//            }else if(arr[i].contains("-")){
+//                value -= 1;
+//            }
+//        }
+//        return value;
+
+
+        int value = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i].equals("++X") || arr[i].equals("X++")){
+                value += 1;
+            } else if (arr[i].equals("--X") || arr[i].equals("X--")) {
+                value -= 1;
+            }
+        }
+        return value;
+    }
 }
--- Lecture-14/PermutationCombition.java
+++ Lecture-14/PermutationCombition.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class PermutationCombition {
+import java.util.Scanner;
+
+public class PermutationCombition {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the 1st word: ");
+        String firstStr = input.next();
+        System.out.print("Enter the 2nd word: ");
+        String secondStr = input.next();
+        input.close();
+        System.out.println(findPermutationDifference(firstStr,secondStr));
+    }
+
+    public static int findPermutationDifference(String s, String t){
+        int sum = 0;
+        for(int i = 0; i<s.length(); i++){
+            sum += Math.abs(i - t.indexOf(s.charAt(i)));
+        }
+        return sum;
+    }
 }
