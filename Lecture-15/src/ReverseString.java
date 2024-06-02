5f307e16dc445de7a9c4dddff7cfb4f32c10e6af
Samiran Samanta <samirans170@gmail.com>
2024-06-02 22:21:29 +0530

    Commit

--- Lecture-15/src/ReverseString.java
+++ Lecture-15/src/ReverseString.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class ReverseString {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of the string array: ");
+        int size = input.nextInt();
+        String[] arr = new String[size];
+        for(int i = 0; i<size; i++){
+            System.out.printf("Enter %d number string: ",i+1);
+            arr[i] = input.next();
+        }
+        input.close();
+        System.out.println(Arrays.toString(arr));
+        reverseString(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void reverseString(String[] arr){
+//        int start = 0;
+//        int end = arr.length-1;
+//        while (start<end){
+//            String temp = arr[start];
+//            arr[start] = arr[end];
+//            arr[end] = temp;
+//            start++;
+//            end--;
+//        }
+
+        int end = arr.length - 1;
+        for(int i = 0; i<arr.length/2; i++){
+            String temp = arr[i];
+            arr[i] = arr[end - i];
+            arr[end - i] = temp;
+        }
+    }
 }
