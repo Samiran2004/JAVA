a44b23b755d596683f318c1b2d52342403c0a2d6
Samiran Samanta <samirans170@gmail.com>
2024-06-04 19:37:10 +0530

    Commit

---
+++ Lecture-15/src/AlternatingSum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class AlternatingSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the digit: ");
+        int digit = input.nextInt();
+        input.close();
+        System.out.println(findAlterDigitSum(digit));
+    }
+
+    public static int findAlterDigitSum(int digit){
+        char operator = '+';
+        int sum = 0;
+        String intStr = Integer.toString(digit);
+        for (int i = 0; i<intStr.length(); i++){
+            int num = Character.getNumericValue(intStr.charAt(i));
+            if(operator == '-'){
+                sum -= num;
+                operator = '+';
+            }else if(operator == '+'){
+                sum += num;
+                operator = '-';
+            }
+        }
+        return sum;
+    }
+}
