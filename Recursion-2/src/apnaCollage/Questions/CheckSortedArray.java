0f17bc4df960f17ee426578f6516ccee200b0f29
Samiran Samanta <130337647+Samiran2004@users.noreply.github.com>
2024-09-09 11:02:29 +0530

    Check Array is Sorted or Not (Strictly)

---
+++ Recursion-2/src/apnaCollage/Questions/CheckSortedArray.java
@@ -0,0 +1,18 @@
+package apnaCollage.Questions;
+
+public class CheckSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println(isSorted(arr,0));
+    }
+    public static boolean isSorted(int[] arr, int idx){
+        if (idx == arr.length - 1){
+            return true;
+        }
+        if (arr[idx] <= arr[idx+1]){
+           return isSorted(arr,idx + 1);
+        }else {
+            return false;
+        }
+    }
+}
